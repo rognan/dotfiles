93e8fd3d2483d842413315c318184a936ee37756
Thor Andreas Rognan <thor.rognan@gmail.com>
2014-06-21 23:36:36 +0200

    Make install-deps.sh platform safe

--- install-deps.sh
+++ install-deps.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 set -u
 
-# homebrew
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go/install)
+case $(uname -s) in
+    Darwin)
+        # install homebrew
+        ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go/install)
 
-# command line spotify
-mkdir ~/code && cd ~/code
-git clone git://github.com/dronir/SpotifyControl.git
-chmod u+x SpotifyControl/SpotifyControl.scpt
+        # command line spotify on mac
+        mkdir ~/code && cd ~/code
+        git clone git://github.com/dronir/SpotifyControl.git
+        chmod u+x SpotifyControl/SpotifyControl.scpt
 
-# for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
+        # for the c alias (syntax highlighted cat)
+        sudo easy_install Pygments
+    ;;
+    *) echo 'No dependencies to be installed on this platform.';;
+esac
